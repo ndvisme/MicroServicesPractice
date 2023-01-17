26a2406ffc0d23e7cabe537a3bd5dd3e0ffbf751
ndvisme <herespier@yahoo.com>
2023-01-18 01:59:37 +0200

    Created Book service.

---
+++ book/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+from routes import book_blueprint
+from models import db, init_app
+from flask_migrate import Migrate
+import os
+file_path = os.path.abspath(os.getcwd())+"./database/book.db"
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '5fPxNUWP2Srzded6fayMeA'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'+ file_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.register_blueprint(book_blueprint)
+init_app(app)
+
+migrate = Migrate(app, db)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5002)
